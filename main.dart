7e8e4a74540bc6b091e3d1ee459a86413fd5846a
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-01-15 14:42:01 +0500

    Updae

--- main.dart
+++ main.dart
@@ -1,4 +1,35 @@
 import 'dart:io';
-void main() {
+import 'student_operations.dart';
 
+void main() {
+  List<Map<String, String>> students = [];
+
+//////////////////  Main menu \\\\\\\\\\\\\\\\\\\\
+
+  while (true) {
+    print("\n---- Welcome to Student Portal ----");
+    print("1: Registration");
+    print("2: Login");
+    print("3: View All Student(Admin)");
+    print("4: Exit");
+
+    stdout.write("choose an option: ");
+
+//////////////  Functions calling and user input with if else \\\\\\\\\\\\
+
+    String? choice = stdin.readLineSync();
+
+    if (choice == 1) {
+      registrationStudent(students);
+    } else if (choice == 2) {
+      loginStudent(students);
+    } else if (choice == 3) {
+      viewAllStudents(students);
+    } else if (choice == 4) {
+      print("Exiting... Goodbye!");
+      break;
+    } else {
+      print("Invalid choice. Please try again.");
+    }
+  }
 }
